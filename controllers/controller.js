e7f0b612166d2e8b48f86b93e97476915383f87f
AsmodayCelestia <herusyeme@gmail.com>
2023-11-15 08:21:16 +0700

    feat:regiter

--- controllers/controller.js
+++ controllers/controller.js
@@ -91,6 +91,21 @@ class Controller{
             res.status(500).json({message: "Internal Server Error"})
         }
     }
+
+    static async register(req, res, next){
+        try {
+            const user = await User.create(req.body)
+            res.status(201).json({id: user.id, email: user.email})
+        } catch (error) {
+            if(error.name === "SequelizeValidationError"){
+                res.status(404).json({message: error.errors[0].message})
+            }else if( error.name === "SequelizeUniqueConstraintError"){
+                res.status(404).json({message: error.errors[0].message})
+            }else{
+                res.status(500).json({message: "Internal Server Error"})
+            }
+        }
+    }
 }
 
 module.exports = Controller 
